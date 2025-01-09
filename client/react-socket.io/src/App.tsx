e4492d9f5d2b621eaa624949780269dd527d66a3
N.I <iworksWeb@gmail.com>
2025-01-09 12:58:18 +0900

    Add chat interface and styling
    - Created App.css for styling the chat interface.
    - Updated App.tsx to include a "Join Chat" section with input fields for name and room number, and a button to join the room.
    - Modified index.css to include global styles for margin, padding, and box-sizing.

--- client/react-socket.io/src/App.tsx
+++ client/react-socket.io/src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <div className="join-chat">
+          <h3 className="join-chat__title">Join Chat</h3>
+          <input
+            className="join-chat__input join-chat__input--name"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="join-chat__input join-chat__input--room"
+            type="text"
+            placeholder="Room Number"
+          />
+          <button className="join-chat__button">Join Room</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
