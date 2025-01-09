46b9f7a9f6c6ef2d24977a73993d031b1736e6ea
N.I <iworksWeb@gmail.com>
2025-01-09 12:36:49 +0900

    Implement Socket.IO integration for real-time user connection handling in the server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 // @Doc https://socket.io/docs/v4/tutorial/step-1
 const express = require("express");
 const { createServer } = require("node:http");
+// @Doc https://socket.io/docs/v4/tutorial/step-3
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-const PORT = 5000;
+io.on("connection", (socket) => {
+  console.log("a user connected" + "socket-id" + socket.id);
 
+  socket.io("disconnect", () => {
+    console.log("a user disconnected" + "socket-id" + socket.id);
+  });
+});
+
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
 });
