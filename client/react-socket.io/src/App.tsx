f42f497e1a79eed89183aa0c74e1002f45846971
N.I <iworksWeb@gmail.com>
2025-01-09 14:30:38 +0900

    Refactor App.tsx to conditionally render chat interface
    - Added state management for showing the chat interface after joining a room.
    - Updated joinRoom function to set the showChat state to true upon successful room joining.
    - Refactored the rendering logic to display the join chat form or the Chat component based on the showChat state.

--- client/react-socket.io/src/App.tsx
+++ client/react-socket.io/src/App.tsx
@@ -8,35 +8,40 @@ const socket = io("http://localhost:5000");
 function App() {
   const [username, SetUserName] = useState<string>("");
   const [room, setRoom] = useState<string>("");
+  const [showChat, setShowChat] = useState<boolean>(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
   return (
     <>
       <div className="app">
-        <div className="join-chat">
-          <h3 className="join-chat__title">Join Chat</h3>
-          <input
-            className="join-chat__input join-chat__input--name"
-            type="text"
-            placeholder="Name"
-            onChange={(e) => SetUserName(e.target.value)}
-          />
-          <input
-            className="join-chat__input join-chat__input--room"
-            type="text"
-            placeholder="Room Number"
-            onChange={(e) => setRoom(e.target.value)}
-          />
-          <button className="join-chat__button" onClick={() => joinRoom()}>
-            Join Room
-          </button>
-        </div>
-        <Chat socket={socket} username={username} room={room} />
+        {!showChat ? (
+          <div className="join-chat">
+            <h3 className="join-chat__title">Join Chat</h3>
+            <input
+              className="join-chat__input join-chat__input--name"
+              type="text"
+              placeholder="Name"
+              onChange={(e) => SetUserName(e.target.value)}
+            />
+            <input
+              className="join-chat__input join-chat__input--room"
+              type="text"
+              placeholder="Room Number"
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button className="join-chat__button" onClick={() => joinRoom()}>
+              Join Room
+            </button>
+          </div>
+        ) : (
+          <Chat socket={socket} username={username} room={room} />
+        )}
       </div>
     </>
   );
